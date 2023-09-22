7c8e8e7773c81df6671231e058e02504a6c69d6e
Aluno <pedro.scabelo2b2@gmail.com>
2023-09-22 14:20:37 -0300

    branch com o código da aula do dia Fri, Sep 22, 2023  2:20:37 PM

---
+++ src/LoginScreen.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { View, Text, TextInput, Button, StyleSheet } from "react-native";
+
+const Login = () => {
+    const[email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const AlterarLogin = () => {
+        console.log(`login com Email: ${email} e senha: ${password}`);
+
+    };
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.heading}> Tela de Login </Text>
+            <TextInput
+                style={styles.input}
+                placeholder="Email"
+                value={email}
+                onChangeText={(text) => setEmail(text)}
+            />
+            <TextInput
+                style={styles.input}
+                placeholder="Senha"
+                secureTextEntry={true}
+                value={password}
+                onChangeText={(text) => setPassword(text)}
+            />
+
+            <Button title="Login" onPress={AlterarLogin} />
+
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    
+    heading: {
+        fontSize: 20,
+        fontWeight:'bold',
+        marginBottom: 20,
+    },
+
+    input: {
+        width: '80%',
+        padding: 20,
+        marginBottom: 20,
+        borderWidth: 1,
+        borderColor: 'gray',
+    },
+});
+
+export default Login;
+
+
